57011ab38665c2e84cc786aefe32fd78a8ee4f4d
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2022-02-13 23:22:29 -0300

    Adicionando a funcionalidade para repetir a operação de busca

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ def mensagem_de_abertura():
 
 
 def start():
-	mensagem_de_abertura()
-
 	cep = input('Digite o CEP que você deseja consultar: (Apenas números)\n')
 
 	if len(cep) != 8:
@@ -18,10 +16,28 @@ def start():
 
 	request = requests.get(f'https://viacep.com.br/ws/{cep}/json')
 
-	print(request.json())
+	address = request.json()
+
+	if 'erro' not in address:
+		print(request.json())
+
+	else:
+		print('CEP inválido')
+		while True:
+			new_try = int(input('Você deseja realizar uma nova consulta? (1) Para SIM e (2) Para NÃO\n'))
+
+			if new_try == 1:
+				start()
+
+			elif new_try == 2:
+				exit()
+
+
+		
 
 invalid_input = True
 
+mensagem_de_abertura()
 
 while invalid_input:
     start()
